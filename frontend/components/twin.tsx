a8a8aaad2a6fc9afcd2f685ec3ea7aa2ba7982f5
svkmdl <souvikmondal15@gmail.com>
2026-02-21 22:59:08 +0100

    chore : updated frontend api url to aws api gateway url

--- frontend/components/twin.tsx
+++ frontend/components/twin.tsx
@@ -40,7 +40,7 @@ export default function Twin() {
         setIsLoading(true);
 
         try {
-            const response = await fetch('http://localhost:8000/chat', {
+            const response = await fetch('https://tgvm5qb41f.execute-api.eu-central-1.amazonaws.com/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
